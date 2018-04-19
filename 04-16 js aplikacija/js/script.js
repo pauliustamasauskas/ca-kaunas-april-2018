b40f93688d68320a42ec71c77e01b1df39cff0a9
Paulius <pauliustamasauskas1@gmail.com>
2018-04-19 18:06:18 +0300

    last

--- 04-16 js aplikacija/js/script.js
+++ 04-16 js aplikacija/js/script.js
@@ -1,26 +1,42 @@
 let product_array = [];
 
-console.log(product_array);
-
-function test(){
-    
-    console.log(typeof product_array);
+function test() {
 
     let newProduct = document.getElementById("txt_product").value;
-    product_array.push(newProduct);
 
-    document.getElementById("result").innerText = product_array;
+    let egzistuoja = false;
+
+    egzistuoja = checkForDuplicates(newProduct, product_array);
+
+    if (egzistuoja) {
+
+        alert("DUPLIKATAS");
+    } else {
+
+        product_array.push(newProduct);
+    }
 
     printProduct(product_array);
 }
 
 
-function printProduct(product_array){ 
+function printProduct(product_array) {
     let h = "";
-    for(let i = 0; i < product_array.length; i++){
-        console.log(i);
-        console.log(product_array[i]);
-        h = h + "<ul><li>"+product_array[i]+"</li></ul>";
+    h = h + "<ul>";
+    for (let i = 0; i < product_array.length; i++) {
+        h = h + "<li>" + product_array[i] + "</li>";
     }
+    h = h + "</ul>";
     document.getElementById("result").innerHTML = h;
 }
+
+function checkForDuplicates(newProduct, product_array) {
+    let found = false;
+    for (let i = 0; i < product_array.length; i++) {
+        if (product_array[i] == newProduct) {
+            found = true;
+        }
+    }
+    return found;
+}
+
